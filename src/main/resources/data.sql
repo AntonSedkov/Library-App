b8730854baf37c9fcd4fe39eea0dccccf7d7d1c4
User from script <S3dk0vAG@gmail.com>
2021-10-28 20:50:36 +0300

    generate and populate H2 DB from schema.sql and data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,21 @@
 DELETE
 FROM users;
-INSERT INTO users (name, password)
-VALUES ('worm', '{noop}pass'),
-       ('fox', '{noop}pass'),
-       ('pig', '{noop}pass');
+INSERT INTO users (id, name, password)
+VALUES (1, 'worm', '{noop}pass'),
+       (2, 'fox', '{noop}pass'),
+       (3, 'pig', '{noop}pass');
 
 DELETE
 FROM books;
-INSERT INTO books (title, author, year, genre, is_ebook)
-VALUES ('Thinking in Java', 'Bruce Eckel', 2006, 'IT', false),
-       ('On the Origin of Species', 'Charles Darwin', 1859, 'SCIENTIFIC', false),
-       ('Ulysses', 'James Joyce', 1918, 'FICTION', false),
-       ('Captivate', 'Vanessa Van Edwards', 2017, 'SOCIAL', false),
-       ('The 7 Habits of Highly Effective People', 'Stephen R. Covey', 1989, 'BUSINESS', false),
-       ('The Collected Poems', 'Sergei Yesenin', 2000, 'POETRY', false),
-       ('War and Peace', 'Leo Tolstoy', 1867, 'FICTION', true),
-       ('Dune', 'Frank Herbert', 1965, 'FICTION', true);
+INSERT INTO books (id, title, author, year, genre, is_ebook)
+VALUES (1, 'Thinking in Java', 'Bruce Eckel', 2006, 'IT', false),
+       (2, 'On the Origin of Species', 'Charles Darwin', 1859, 'SCIENTIFIC', false),
+       (3, 'Ulysses', 'James Joyce', 1918, 'FICTION', false),
+       (4, 'Captivate', 'Vanessa Van Edwards', 2017, 'SOCIAL', false),
+       (5, 'The 7 Habits of Highly Effective People', 'Stephen R. Covey', 1989, 'BUSINESS', false),
+       (6, 'The Collected Poems', 'Sergei Yesenin', 2000, 'POETRY', false),
+       (7, 'War and Peace', 'Leo Tolstoy', 1867, 'FICTION', true),
+       (8, 'Dune', 'Frank Herbert', 1965, 'FICTION', true);
 
 DELETE
 FROM books_in_use;
