92bee56b7f7478b94444fca048ce557a7dbfe74d
User from script <S3dk0vAG@gmail.com>
2021-10-28 21:34:08 +0300

    WebConfig + simple index page + css

---
+++ src/main/java/by/anthony/library/config/WebConfig.java
@@ -0,0 +1,18 @@
+package by.anthony.library.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login");
+    }
+}
